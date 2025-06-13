15b142ad759242c7872511f72f2aac85493b1600
Harry Kalsted <47069573+hinrichk44@users.noreply.github.com>
2025-06-13 13:04:27 -0400

    Add files via upload

--- 02_process_data.R
+++ 02_process_data.R
@@ -2,59 +2,35 @@ separate_bike_model <-
 function(data, keep_model_column = TRUE, append = TRUE) {
     
     
-    # "if not append, then simply select model column from our data"
-    if (!append) {data <- data %>% select(model)}
+    # "if append = FALSE, then simply select the model column from our data"
+    if (!append) {
+        
+        data <- data %>% select(model)
+        
+        }
     
-    
-    # 2.2 Process Model Column ----
-    # Fix typo
+    # "else, separate the model column into the code below"
     output_tbl <- data %>% 
         mutate(model = case_when(
             model == "CAAD Disc Ultegra" ~ "CAAD12 Disc Ultegra",
             model == "Syapse Carbon Tiagra" ~ "Synapse Carbon Tiagra",
             model == "Supersix Evo Hi-Mod Utegra" ~ "Supersix Evo Hi-Mod Ultegra",
-            TRUE ~ model
-    )) %>%
-        
-        # Separate "model" column using spaces
-        # We will now get model_1, model_2, etc up until model_7
+            TRUE ~ model)) %>%
         separate(col     = model, 
                  into    = str_c("model_", 1:7), 
                  sep     = " ", 
                  remove  = FALSE, 
                  fill    = "right") %>%
-        
-        # Creating a "base" feature
-        mutate(model_base = case_when(
-            
-            # Fix Supersix Evo
-            str_detect(str_to_lower(model_1), "supersix") ~ str_c(model_1, model_2, sep = " "),
-            
-            # Fix Fat CAAD bikes
-            str_detect(str_to_lower(model_1), "fat") ~ str_c(model_1, model_2, sep = " "),
-            
-            # Fix Beast of the East
-            str_detect(str_to_lower(model_1), "beast") ~ str_c(model_1, model_2, model_3, model_4, sep = " "),
-            
-            # Fix Bad Habit
-            str_detect(str_to_lower(model_1), "bad") ~ str_c(model_1, model_2, sep = " "),
-            
-            # Fix Scalpel 29
-            str_detect(str_to_lower(model_2), "29") ~ str_c(model_1, model_2, sep = " "),
-            
-            # catch all
-            TRUE ~ model_1)
-        ) %>%
-        
-        # Get "tier" feature
+        mutate(
+            model_base = case_when(
+                str_detect(str_to_lower(model_1), "supersix") ~ str_c(model_1, model_2, sep = " "),
+                str_detect(str_to_lower(model_1), "fat") ~ str_c(model_1, model_2, sep = " "),
+                str_detect(str_to_lower(model_1), "beast") ~ str_c(model_1, model_2, model_3, model_4, sep = " "),
+                str_detect(str_to_lower(model_1), "bad") ~ str_c(model_1, model_2, sep = " "),
+                str_detect(str_to_lower(model_2), "29") ~ str_c(model_1, model_2, sep = " "),
+                TRUE ~ model_1)) %>%
         mutate(model_tier = model %>% str_replace(model_base, replacement = "") %>% str_trim()) %>%
-        
-        # Remove unnecessary columns
         select(-matches("model_[0-9]")) %>%
-        
-        # Create Flags
-        # So here we are creating a numeric value for each time one of these flags appear in the Bike Models
-        # So all of these flags will become individual columns with numerical values in the rows
         mutate(
             black     = model_tier %>% str_to_lower() %>% str_detect("black") %>% as.numeric(),
             hi_mod    = model_tier %>% str_to_lower() %>% str_detect("hi-mod") %>% as.numeric(),
@@ -67,7 +43,7 @@ function(data, keep_model_column = TRUE, append = TRUE) {
     
     
     
-    # "if not keep model column, then remove model column from output_tbl"
+    # "if keep_model_column = FALSE, then remove model column from output_tbl"
     if (!keep_model_column) output_tbl <- output_tbl %>% select(-model)
     
     
@@ -78,21 +54,27 @@ separate_bike_description <-
 function(data, keep_description_column = TRUE, append = TRUE) {
     
     
-    # "if not append, then simply select description column from our data"
-    if (!append) {data <- data %>% select(description)}
-   
-    
-     # 2.1 Separate Description Column ----
+    # "if append = FALSE, then simply select description column from our data as our output"
+    if (!append) {
+        
+        data <- data %>% select(description)
+
+    } 
+      
+      
+    # "else, if append = TRUE then separate the description column into "category_1", "category_2", and "frame_material" and keep rest of data too"
     output_tbl <- data %>% separate(description, 
-             sep    = " - ", 
-             # Category 1 = "bike type" and Category 2 = "bike family
-             into   = c("category_1", "category_2", "frame_material"), 
-             remove = FALSE)
-    
-    
-    # "if not keep description column, then remove description column from output_tbl"
+                                    sep    = " - ", 
+                                    into   = c("category_1", "category_2", "frame_material"), 
+                                    remove = FALSE)
+        
+   
+    # "if keep_description_column = FALSE, then remove description column from output_tbl"
     if (!keep_description_column) output_tbl <- output_tbl %>% select(-description)
     
+    
+    # The outcome of our function is output_tbl
+    # So, "data" comes in, and "output_tbl" comes out
     return(output_tbl)
     
 }
